0ef83fed68f860da3ae7e81f0c050bc7888f940b
João Vitor <jvdrdo@gmail.com>
2024-09-22 21:09:35 -0300

    Update ProductTester.java

--- src/inventario/ProductTester.java
+++ src/inventario/ProductTester.java
@@ -1,19 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ProductTester {
+
     public static void main(String[] args) {
 
-        Produto notebook = new Produto("Notebook", 1, 10, 3200);
-        Produto celular = new Produto("Celular", 2, 10, 2400);
-        Produto fone = new Produto("Fone", 3, 10, 120);
-        Produto carregador = new Produto("Carregador", 4, 10, 65.70);
-        Produto teclado = new Produto("Teclado", 5, 10, 124.56);
-        Produto mouse = new Produto("Mouse", 6, 10, 110.45);
+        Scanner teclado = new Scanner(System.in);
 
-        System.out.println(notebook.toString());
-        System.out.println(celular.toString());
-        System.out.println(fone.toString());
-        System.out.println(carregador.toString());
-        System.out.println(teclado.toString());
-        System.out.println(mouse.toString());
+        Usuario usuario = null;
+        List<Produto> carrinho = new ArrayList<>();
+
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("Notebook", 1, 100, 2500));
+        produtos.add(new Produto("Celular", 2, 100, 1500));
+        produtos.add(new Produto("Fone", 3, 100, 340));
+        produtos.add(new Produto("Carregador", 4, 100, 78.45));
+        produtos.add(new Produto("PC", 5, 100, 5600));
+        produtos.add(new Produto("Monitor", 6, 100, 3000));
+
+        int opcao = 0;
+
+        while (opcao != 5) {
+            System.out.println("\n 1-Criar conta \n 2-Carrinho de compra \n 3-Comprar produto \n 4-Conta \n 5-Sair");
+            opcao = teclado.nextInt();
+            System.out.println(" ");
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("NOME: ");
+                    String nome = teclado.next();
+
+                    System.out.print("IDADE: ");
+                    int idade = teclado.nextInt();
+
+                    System.out.print("SALDO: ");
+                    double saldo = teclado.nextDouble();
+
+                    usuario = new Usuario(idade, nome, saldo);
+                    break;
+                case 2:
+
+                    double valor = carrinho.stream().mapToDouble(Produto::getValor).sum();
+
+                    carrinho.forEach(System.out::println);
+                    System.out.println("TOTAL: " + valor);
+
+                    System.out.println("Pagar carrinho: 1-Sim 2-Não");
+                    int pagar = teclado.nextInt();
+
+                    if (pagar == 1) {
+                        usuario.setSaldo(usuario.getSaldo()-valor);
+                    }
+                    break;
+                case 3:
+
+                    produtos.forEach(System.out::println);
+
+                    System.out.print("SELECIONE O ID DO PRODUTO: ");
+                    pagar = teclado.nextInt();
+
+                    usuario.setCarrinhoProduto(produtos.stream().filter(produto -> produto.getNumeroItem() == pagar).toList());
+                    break;
+                case 4:
+                    System.out.println(usuario.toString());
+                    break;
+            }
+        }
     }
 }
